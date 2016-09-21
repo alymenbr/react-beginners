327cc91a7000225ebdf23dcd640cc2ec1f2e686c
Alysson Anthony <alymenbr@gmail.com>
2016-09-21 18:13:08 -0300

    ml2-w3

--- catch-of-the-day/scripts/main.js
+++ catch-of-the-day/scripts/main.js
@@ -10,6 +10,10 @@ var createBrowserHistory = require('history/lib/createBrowserHistory')
 
 var h = require('./helpers');
 
+// Firebase
+var Rebase = require('re-base');
+var base = Rebase.createClass('alymenbr-react-begin.firebaseio.com');
+
 
 /*
   App
@@ -21,6 +25,11 @@ var App = React.createClass({
       fishes: {},
       order: {}
     }
+  },
+
+  addToOrder: function (key) {
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({order: this.state.order});
   },
 
   addFish: function (fish) {
@@ -36,7 +45,7 @@ var App = React.createClass({
   },
 
   renderFish: function (key) {
-    return <Fish key={key} index={key} details={this.state.fishes[key]} />
+    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
   },
 
   render: function() {
@@ -48,7 +57,7 @@ var App = React.createClass({
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order/>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
@@ -61,11 +70,24 @@ var App = React.createClass({
   <Fish />
 */
 var Fish = React.createClass({
+  onButtonClick: function () {
+    var key = this.props.index;
+    this.props.addToOrder(key);
+  },
+
   render: function () {
     var details = this.props.details;
+    var isAvailable = (details.status == 'available'? true : false);
+    var buttonText = (isAvailable? 'Add to Order' : 'Sold Out!');
     return (
-      <li class="menu-fish">
-        <img src={details.image} alt="" />
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{h.formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
       </li>
     )
   }
@@ -137,9 +159,50 @@ var Header = React.createClass({
   Order
 */
 var Order = React.createClass({
-  render: function () {
+
+  renderOrder: function (key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if(!fish) {
+      return <li key={key}>Sorry, fish unavailable!</li>
+    }
+
     return (
-      <p>Order</p>
+      <li>
+        <span>{count}kg de </span>
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
+
+  render: function () {
+
+    var orderIds = Object.keys(this.props.order);
+    var total = orderIds.reduce(  (prevTotal, key) => {
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if(fish && isAvailable){
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
+    return (
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
+          <li className="total">
+            <strong>Total:</strong>
+            {h.formatPrice(total)}
+          </li>
+        </ul>
+      </div>
     )
   }
 });
